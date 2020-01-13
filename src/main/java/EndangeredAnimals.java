ea04e4ebae64148e297c2a3cffe547ac711857c8
Joelmukono <jlmukono@gamil.com>
2020-01-13 21:59:22 +0300

    add endangered animals

--- src/main/java/EndangeredAnimals.java
+++ src/main/java/EndangeredAnimals.java
@@ -8,4 +8,55 @@ public class EndangeredAnimals {
     private String name;
     private String health;
 
+    public String getName(){
+        return name;
+
+    }
+
+    public String getHealth(){
+        return health;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object otherPerson){
+        if (!(otherPerson instanceof Animal )) {
+            return false;
+        } else {
+            Animal newPerson = (Animal) otherPerson;
+            return this.getName().equals(newPerson.getName());
+        }
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO endangered (name) VALUES (:name)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    public static List<Animal> all() {
+        String sql = "SELECT * FROM endangered";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Animal.class);
+        }
+    }
+
+    public void update(int id, String newName){
+        String sql = "UPDATE endangered SET name = :name WHERE id=:id";
+        try(Connection con = DB.sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("name", newName)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
 }
