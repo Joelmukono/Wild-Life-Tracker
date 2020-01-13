fa5c2388abd89f4b7b70d198b817bb907c0d0998
Joelmukono <jlmukono@gamil.com>
2020-01-13 16:30:04 +0300

    add Endangered animal class

---
+++ src/main/java/EndangeredAnimals.java
@@ -0,0 +1,11 @@
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class EndangeredAnimals {
+    private int id;
+    private String name;
+    private String health;
+
+}
