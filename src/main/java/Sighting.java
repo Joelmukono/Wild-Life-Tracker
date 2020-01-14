4a9c4bb7cadfe262238f44bfa8a6d1e9f9a5fd77
Joelmukono <jlmukono@gamil.com>
2020-01-14 08:20:22 +0300

    add database logic to sighting class

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -1,25 +1,41 @@
 import org.sql2o.Connection;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 public class Sighting implements Sightings {
     private int id;
     private String location;
     private String rangerName;
+    private Timestamp created;
+
+    public Sighting(String location,String rangerName){
+        this.location = location;
+        this.rangerName = rangerName;
+
+    }
 
 
     @Override
     public void save() {
         try(Connection con = DB.sql2o.open()) {
-            String sql = "INSERT INTO communities (name, description) VALUES (:name, :description)";
+            String sql = "INSERT INTO sightings (location, rangername,created) VALUES (:location, :rangerName,now())";
             this.id = (int) con.createQuery(sql, true)
-                    .addParameter("name", this.name)
-                    .addParameter("description", this.description)
+                    .addParameter("name", this.location)
+                    .addParameter("description", this.rangerName)
+                    .addParameter("date",this.created)
                     .executeUpdate()
                     .getKey();
         }
     }
 
-    @Override
-    public void all() {
+
+    public static List<Sighting> all() {
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Sighting.class);
+        }
+    }
 
     }
 }
--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -3,5 +3,5 @@
 public interface Sightings {
 
     void save();
-    void all();
+
 }
